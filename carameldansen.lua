d47a638db8d9ec2ba653bc75db9cc74996069a75
Max_Gorn <114423601+MaxonIgla@users.noreply.github.com>
2026-05-10 00:21:55 +0300

    Update carameldansen.lua

--- carameldansen.lua
+++ carameldansen.lua
@@ -1,16 +1,16 @@
 local dfpwm = require("cc.audio.dfpwm")
 
 local speaker = peripheral.find("speaker")
-local volume = 0.5
+local volume = 0.7
 
 
 local decoder = dfpwm.make_decoder()
-for chunk in io.lines(arg[1], 16 * 1024) do
+for chunk in io.lines(arg[1], 1 * 1024 / 2) do
     local buffer = decoder(chunk)
     if (redstone.getInput("bottom")) then
-        volume = 1.5
+        volume = 2.6
     else
-        volume = 0.5
+        volume = 0.7
     end
     while not spk.playAudio(buffer, volume) do
         os.pullEvent("speaker_audio_empty")
